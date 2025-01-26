200579b57032b9aef877891cc8622dbdae7ed35d
williammabotjaeng <williammabotjaeng@gmail.com>
2025-01-26 11:41:06 +0200

    build and bundle

--- src/auth.ts
+++ src/auth.ts
@@ -15,12 +15,10 @@ export const hasRole = (decodedToken: any, requiredRole: string) => {
   return decodedToken && decodedToken.role === requiredRole;
 };
 
-// Function to generate a new token
 export const generateToken = (userData: any) => {
   return jwt.sign(userData, SECRET_KEY, { expiresIn: '1h' });
 };
 
-// Function to refresh a token (if needed)
 export const refreshToken = (oldToken: string) => {
   const { valid, decoded } = verifyToken(oldToken);
   if (valid) {
